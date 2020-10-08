72122c722c4183a8433b55671fe0121138a3ef3d
nodmentor2ygn <nodmentor2ygn@gmail.com>
2020-10-08 23:36:55 +0630

    create_IGclone

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,10 @@
+class FavoritesController < ApplicationController
+  def create
+    favorite = current_user.favorites.create(picture_id: params[:picture_id])
+    redirect_to pictures_url, notice: "Registered #{favorite.picture.user.name}'s blog as a favorite"
+  end
+  def destroy
+    favorite = current_user.favorites.find_by(id: params[:id]).destroy
+    redirect_to pictures_url, notice: "Unfavorite #{favorite.picture.user.name}'s blog"
+  end
+end
