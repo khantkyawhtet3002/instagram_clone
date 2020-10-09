919d0f45627709f65cd54d1314babee18bda1809
khantkyawhtet3002 <69779607+khantkyawhtet3002@users.noreply.github.com>
2020-10-09 13:36:21 +0630

    favorite_contro

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,6 +3,7 @@ class FavoritesController < ApplicationController
     favorite = current_user.favorites.create(picture_id: params[:picture_id])
     redirect_to pictures_url, notice: "Registered #{favorite.picture.user.name}'s blog as a favorite"
   end
+  
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
     redirect_to pictures_url, notice: "Unfavorite #{favorite.picture.user.name}'s blog"
